7e7dd345284dddbba7ef309df67e531a7804e2d9
Shaid Islam <shaid85@gmail.com>
2024-03-30 16:49:07 +0600

    01. CreateShopping Cart Layout

---
+++ app/(front)/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="text-2xl py-2">Latest Products</h2>
+    </>
+  )
+}
---
+++ components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer footer-center p-4 bg-base-300 text-base-content">
+      <p>Copyright © 2023 - All right reserved by Next Amazona V2</p>
+    </div>
+  )
+}
+
+export default Footer
