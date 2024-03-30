bc8f38976a01469fa1895c93b9e179ab70b9520f
Shaid Islam <shaid85@gmail.com>
2024-03-30 22:42:30 +0600

    02. adding product showcase on home page

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,3 +1,5 @@
+import ProductItem from '@/components/products/ProductItem'
+import data from '@/lib/data'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -5,6 +7,11 @@ export default function Home() {
   return (
     <>
       <h2 className="text-2xl py-2">Latest Products</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
     </>
   )
 }
