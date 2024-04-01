60c1dc11a5d56784d47fca6339714845deb6b785
Shaid Islam <shaid85@gmail.com>
2024-04-02 01:58:47 +0600

    add mongodb and show products

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -3,13 +3,29 @@ import Image from 'next/image'
 import React from 'react'
 import data from '@/lib/data'
 import AddToCart from '@/components/products/AddToCart'
+import productService from '@/lib/services/productService'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  }
+}
 
 export default async function ProductDetails({
   params,
 }: {
   params: { slug: string }
 }) {
-  const product = data.products.find((x) => x.slug === params.slug)
+  const product = await productService.getBySlug(params.slug)
   if (!product) {
     return <div>Product not found</div>
   }
